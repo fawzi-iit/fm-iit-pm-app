e2c4467fa558bd55e8e73181dc81e5722e650e8c
Fawzi Mahmoud <fmahmoud@hawk.iit.edu>
2023-11-30 14:01:18 -0600

    code update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 const bodyParser = require('body-parser');
 
 // Import routes
