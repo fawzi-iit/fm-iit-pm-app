48a8a7a0bf610bb3c0deb44f17cbcf44e99f6849
Fawzi Mahmoud <fmahmoud@hawk.iit.edu>
2023-11-30 15:28:29 -0600

    Update code

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             // Add other mongoose options here as needed
